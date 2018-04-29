6cf0feafb5ea4c64672d025ab203df58e2992df1
dings <xyz031702@hotmail.com>
2018-04-29 12:11:24 +0800

    save

---
+++ order_system/models.py
@@ -0,0 +1,14 @@
+class Admin(object):
+    id=0
+
+class Customer(object):
+    id=0
+
+class Product(object):
+    id=0
+
+class Order(object):
+    id=0
+
+class Invoice(object):
+    id=0
---
+++ order_system/order.py
@@ -0,0 +1,18 @@
+class Actions(object):
+    def create_order(self):
+        return False
+
+    def create_similar_order(self):
+        return False
+
+    def update_order(self):
+        return False
+
+    def del_order(self):
+        return False
+
+    def get_orders(self):
+        return False
+
+    def create_invoice(self):
+        
